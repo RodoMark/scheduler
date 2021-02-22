68bc248d9181c01eaf12e538d70ba05a93f36f8f
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-02-22 17:08:08 -0500

    add initial useReducer code

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,8 +23,9 @@ export default function Form(props) {
     if (name === "") {
       setError("Student name cannot be blank");
       return;
-    }
-  
+    } 
+    
+    setError("")
     props.onSave(name, interviewer);
   }
 
